fdfdbc06abbf20808c85cd41716746e211a6eca6
diorgnes <diodev.@gmail.com>
2025-05-07 15:53:32 -0300

    alteraçoes

--- ValidationSystem/src/Processoseletivo.java
+++ ValidationSystem/src/Processoseletivo.java
@@ -1,12 +1,35 @@
-
+import  java.util.concurrent.ThreadLocalRandom;
 public class Processoseletivo {
     public static void main(String[] args) {
-        System.err.println("Processo Seletivo");
-        analisarCandidatos(1900.0);
-        analisarCandidatos(2200.0);
-        analisarCandidatos(2000.0);
+        selecaodeCandidatos();
+   
 
     }
+
+    static void selecaodeCandidatos(){
+        String [] candidatos = {"FELIPE", "JOAO", "MARIA", "JULIA", "PAULO", "ANA", "CARLOS", "LUCAS", "MARCOS", "GUSTAVO"};
+
+        int candidatosSelecionados = 0;
+        int candidatoAtual = 0;
+        double salarioBase = 2000.0;
+        while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length){
+            String candidato = candidatos[candidatoAtual];
+            double salarioPretendido = valorPretendido();
+
+            System.out.println("O Candidato: " + candidato + " Solicitou este valor : " + salarioPretendido);
+            if(salarioBase >= salarioPretendido){
+                System.err.println("O Candidato: " + candidato + " foi selecionado para a proxima fase do processo seletivo");
+                candidatosSelecionados++;
+            }
+            candidatoAtual++;
+
+         }
+      
+        }
+        static double valorPretendido(){
+            return ThreadLocalRandom.current().nextDouble(1800.0, 2000.0);
+    }
+   
     static void analisarCandidatos(double salarioPretendido) {
         double salarioBase = 2000.0;
         if(salarioBase > salarioPretendido){
